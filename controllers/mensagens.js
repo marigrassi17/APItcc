90828995297a19fe4262e867b74c3dd3235d86a9
MARIANI GRASSI ARAUJO SILVA <mariani.silva6>
2024-09-30 11:36:02 -0300

    aula 2

--- controllers/mensagens.js
+++ controllers/mensagens.js
@@ -3,10 +3,18 @@ const db = require('../database/connection');
 module.exports = {
     async listarMensagens(request, response) {
         try {            
+            // instruçoês SQL 
+            const sql = ` SELECT
+              men_cod, men_texto, men_data, usu_cod,
+              agd_cod, men_visualizada
+              FROM usuarios;` ;
+              // Executa instruçoes SQL e armazena o resultado na variavel usuarios
+              const usuarios = await db. query(sql);
+
             return response.status(200).json({
                 sucesso: true, 
                 mensagem: 'Lista de mensagens.', 
-                dados: null
+                dados: mensagens
             });
         } catch (error) {
             return response.status(500).json({
