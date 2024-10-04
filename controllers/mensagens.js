dd499d71b270505bd32789b9c043ea0992bf4bf5
MARIANI GRASSI ARAUJO SILVA <mariani.silva6>
2024-10-04 11:33:19 -0300

    aula4

--- controllers/mensagens.js
+++ controllers/mensagens.js
@@ -27,11 +27,32 @@ module.exports = {
 
     async cadastrarMensagens(request, response) {
         try {            
+
+         // parametros recebidos no corpo da requisiçao 
+         const {men_texto, men_data, usu_cod, agd_cod, men_visualizada} = request.body;
+
+         // instruçao SQL
+         const sql = `INSERT INTO mensagens
+            (men_texto, men_data, usu_cod, agd_cod, men_visualizada)
+             VALUES (?,?,?,?,?)`;
+
+        //definiçao dos dados a serem inseridos em um array
+        const values = [men_texto, men_data, usu_cod, agd_cod, men_visualizada];
+
+        //execuçao da instruçao sql passando os parametros
+        const execSql = await db.query(sql, values);
+
+        //indentificaçao do ID do registro inserido 
+        const men_cod = execSql[0].insertId;
+
             return response.status(200).json({
                 sucesso: true, 
                 mensagem: 'Cadastrar mensagens.', 
-                dados: null
+                dados: men_cod
+                //mensSql: execSql
             });
+
+            
         } catch (error) {
             return response.status(500).json({
                 sucesso: false,
